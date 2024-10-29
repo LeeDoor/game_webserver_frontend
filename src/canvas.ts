5686162a3fb85101df599745e2471bd453a52bd9
Leonid Samoshenko <samoshenkoleonid@yandex.ru>
2024-10-29 18:58:51 +0300

    global_canvas

---
+++ src/canvas.ts
@@ -0,0 +1,15 @@
+export let canvas : HTMLCanvasElement;
+export let ctx : CanvasRenderingContext2D;
+export let style : CSSStyleDeclaration;
+
+// loads canvas. should be called at the program startup.
+export function loadCanvas(after?: ()=>void) {
+    document.addEventListener("DOMContentLoaded", (event) => {
+        canvas = document.getElementById("canvas") as HTMLCanvasElement;
+        ctx  = canvas.getContext("2d");
+        style = getComputedStyle(canvas);
+        canvas.width = parseFloat(style.width);
+        canvas.height = parseFloat(style.height);
+        if(after) after();
+    });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,10 @@
-
+import * as Screen from "./canvas.js";
 import {Vector2} from "./vector2.js";
 import {Viewport} from "./viewport.js";
 
-
-const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-const ctx = canvas.getContext("2d");
-const abobus = new Image();
-const style = getComputedStyle(canvas);
-canvas.width = parseFloat(style.width);
-canvas.height = parseFloat(style.height);
-abobus.src = "abobus.png";
-
-let viewport : Viewport = new Viewport(canvas);
-
-abobus.addEventListener("load", function (e) {
-    for(let i = 0; i < 5; i++) {
-        setTimeout(function(){
-            ctx.clearRect(0,0,canvas.width, canvas.height);
-            viewport.position = new Vector2(i * 100, i * 100);
-            viewport.drawImage(abobus, new Vector2(0, 0)); 
-        },i * 1000);
-    }
+Screen.loadCanvas(function (){
+    Screen.ctx.beginPath();
+    Screen.ctx.arc(50, 50, 50, 0, Math.PI * 2);
+    Screen.ctx.fillStyle = 'green';
+    Screen.ctx.fill();
 });
