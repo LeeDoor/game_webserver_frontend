a38e6bdd04ff8719c1e27640f67a0fc20ec3834b
Leonid Samoshenko <samoshenkoleonid@yandex.ru>
2024-10-29 14:28:03 +0300

    viewport moves holaaa

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
-import { Viewport } from "./viewport.js";
-import { Vector2 } from "./vector2.js";
 
-function initializeMap() {
-    const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-    const ctx = canvas.getContext("2d");
-    const style = getComputedStyle(canvas);
-    canvas.width = parseFloat(style.width);
-    canvas.height = parseFloat(style.height);
+import {Vector2} from "./vector2.js";
+import {Viewport} from "./viewport.js";
 
-    // let vp : Viewport = new Viewport();
-    // vp.position = new Vector2(0,0);
-    // let abobus = new Image();
-    // abobus.src = "abobus.png";
-    // ctx.drawImage(abobus, 50, 50);
-    console.log(5); 
-}
-addEventListener("DOMContentLoaded", initializeMap);
+
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+const abobus = new Image();
+const style = getComputedStyle(canvas);
+canvas.width = parseFloat(style.width);
+canvas.height = parseFloat(style.height);
+abobus.src = "abobus.png";
+
+let viewport : Viewport = new Viewport(canvas);
+
+abobus.addEventListener("load", function (e) {
+    for(let i = 0; i < 5; i++) {
+        setTimeout(function(){
+            ctx.clearRect(0,0,canvas.width, canvas.height);
+            viewport.position = new Vector2(i * 100, i * 100);
+            viewport.drawImage(abobus, new Vector2(0, 0), new Vector2(500 + i * 100, 600)); 
+        },i * 1000);
+    }
+});
--- src/viewport.ts
+++ src/viewport.ts
@@ -1,6 +1,19 @@
-import { Vector2 } from "vector2";
+import {Vector2} from "./vector2.js";
 
 export class Viewport {
     position: Vector2;
     size: Vector2;
+    readonly ctx: CanvasRenderingContext2D;
+    constructor(canvas: HTMLCanvasElement, position: Vector2 = new Vector2(0, 0)) {
+        this.position = position;
+        this.size = new Vector2(canvas.width, canvas.height);
+        this.ctx = canvas.getContext('2d');
+    }
+    drawImage(image: HTMLImageElement, position: Vector2, size: Vector2 = new Vector2(0, 0)) {
+        if(size.x == 0 && size.y == 0) {
+            size.x = image.width;
+            size.y = image.height;
+        }
+        this.ctx.drawImage(image, position.x - this.position.x, position.y - this.position.y, size.x, size.y);
+    }
 }
